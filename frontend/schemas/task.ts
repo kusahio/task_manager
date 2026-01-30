ca01ed9ce892e267c74e1dfd727ee91599d35e7b
CamiloIllanes <67984533+kusahio@users.noreply.github.com>
2026-01-30 15:41:36 -0300

    añadido tambien que la descripcion no puede ser con espacios en blanco

--- frontend/schemas/task.ts
+++ frontend/schemas/task.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const taskSchema = z.object({
   title: z.string().trim().min(1, 'El título es obligatorio'),
-  description: z.string().optional(),
+  description: z.string().trim().optional(),
   tags: z.array(z.number()).optional(),
   deadline: z.string().optional(),  
 })
